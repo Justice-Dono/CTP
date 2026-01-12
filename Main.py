43733eb765a07a4bb1c2fedb6af18df2fec74022
Justice-Dono <jordan.tewnion@gmail.com>
2026-01-12 14:19:44 -0700

    Screen now works for real

--- Main.py
+++ Main.py
@@ -1,7 +1,20 @@
 import turtle
 
-window = turtle.Screen()
-window.title("Combat Screen")
 
-combat_turtle = turtle.Turtle()
-combat_turtle.write("Hello World!")
+def main():
+	window = turtle.Screen()
+	window.title("Combat Window")
+
+	combat_turtle = turtle.Turtle()
+	combat_turtle.hideturtle()
+	combat_turtle.penup()
+	combat_turtle.goto(0, 0)
+	style = ("Arial", 36, "bold")
+	combat_turtle.write("Hello World", align="center", font=style)
+
+	window.exitonclick()
+
+
+if __name__ == "__main__":
+	main()
+
